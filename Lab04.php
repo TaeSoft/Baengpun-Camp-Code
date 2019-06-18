e606c91e24b71b6cebc131fe11b3bac1af3645cd
TaeSoft <sightydev@gmail.com>
2019-06-18 11:30:27 +0700

    new file:   Lab00-code.txt 	new file:   Lab00.html 	new file:   Lab01-code.txt 	new file:   Lab01.html 	new file:   Lab02-code.txt 	new file:   Lab02.html 	new file:   Lab03-code.txt 	new file:   Lab03.html 	new file:   Lab04-code.txt 	new file:   Lab04.php 	new file:   Lab05-code.txt 	new file:   Lab05.php 	new file:   geothailand.sql

---
+++ Lab04.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Lab04: Basic PHP</title>
+	
+	<style>
+	h3 {
+	
+		animation: result 10s linear infinite alternate;
+	}
+	
+	@keyframes result {
+	0%   { 
+
+		transform: translateX(70%); 		
+	}
+	100% { 
+
+		transform: translateX(30%); 
+	}
+	</style>
+</head>
+<body>
+	<h1 style="text-align: center; margin: 0 auto;">ทดสอบ PHP</h1>
+	<h3><span>...</span></h3>
+	<hr />
+	<div style="text-align: center;">
+		<form method="post">
+			<input type="text" name="input_value">
+			<button type="submit">เปลี่ยนเลยยย!</button>
+		</form>
+	</div>
+</body>
+</html>
---
+++ Lab05.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<th>Province ID</th>
+			<th>Province Name</th>
+		</tr>
+	</table>
+</body>
+</html>
